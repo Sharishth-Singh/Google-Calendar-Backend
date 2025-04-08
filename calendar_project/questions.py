5b481e29969fbebe9e7286951efe8c85d65f2e16
Sharishth-Singh <sharishthsingh@gmail.com>
2025-04-08 12:34:37 +0530

    added editorial questions

--- calendar_project/questions.py
+++ calendar_project/questions.py
@@ -8,7 +8,21 @@ from bs4 import BeautifulSoup
 
 def get_pwonlyias_questions_by_date():
     session = requests.Session()
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Cache-Control": "max-age=0",
+        "Sec-Ch-Ua": '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"Windows"',
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "cross-site",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1"
+    }
     target_date = datetime.date(2025, 4, 7)
     # target_date = datetime.date.today()
 
@@ -50,9 +64,6 @@ def get_pwonlyias_questions_by_date():
     url = "https://pwonlyias.com/mains-answer-writing/page/0"
     response = session.get(url, headers=headers)
     html_content = response.content.decode("utf-8")
-
-    with open("pw_response.html", "w", encoding="utf-8") as f:
-        f.write(response.text)
 
     questions = extract_questions_with_tags(html_content, target_date)
     return questions
